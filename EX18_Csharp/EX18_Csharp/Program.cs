1558d4b91e23056b13ef0e318e5f9f7ff134cb77
ChuManhCuong312 <146830932+ChuManhCuong312@users.noreply.github.com>
2024-06-18 18:07:45 +0700

    Add files via upload

---
+++ EX18_Csharp/EX18_Csharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        //Gọi hàm để đọc vào số x
+        double x = ReadDouble8Byte();
+        double sqrtX = Math.Sqrt(x);
+        //Hiển thị giá trị căn bậc 2 của x
+        Console.WriteLine($"Căn bậc 2 của {x} là: {sqrtX}");
+    }
+
+    // Hàm để đọc số thực 8 byte từ bàn phím và kiểm tra tính hợp lệ
+    static double ReadDouble8Byte()
+    {
+        double result;
+        Console.Write("Nhập số thực 8 byte x: ");
+
+        while (!double.TryParse(Console.ReadLine(), out result))
+        {
+            Console.Write("Nhập sai, vui lòng nhập lại số thực 8 byte: ");
+        }
+
+        return result;
+    }
+}
